4a8112f6ea1d04025c0e106a262f33f8e543a6f2
Annika L <annika0uwu@gmail.com>
2021-12-24 13:08:38 -0800

    Finish `/<pollid>/vote` API endpoint

--- src/api.rs
+++ src/api.rs
@@ -1,7 +1,10 @@
 //! BetterVote API endpoints
 
+use std::net::IpAddr;
+
 use rocket::serde::json::{json, Json, Value};
 use rocket::serde::{Deserialize, Serialize};
+use rocket::tokio::net::unix::SocketAddr;
 
 use crate::database::postgres::PostgresConnection;
 use crate::error::ErrorKind;
@@ -28,14 +31,21 @@ fn handle_error(e: ErrorKind) -> Value {
 }
 
 #[post("/<pollid>/vote", data = "<data>")]
-async fn vote(conn: PostgresConnection, pollid: String, data: Json<VoteAPIRequestData>) -> Value {
+async fn vote(mut conn: PostgresConnection, pollid: String, data: Json<VoteAPIRequestData>, remote_addr: Option<IpAddr>) -> Value {
     let Json(request) = data;
-    let vote = RankedChoiceVote {
-        ranked_choices: request.choices,
-        voter_ip: todo!(),
+    let voter_ip = match remote_addr {
+        Some(ip) => ip,
+        None => {
+            return json!({ "error": "No IP address was provided.", "success": false });
+        }
     };
 
-    let poll = match conn.get_poll_by_id(pollid).await {
+    let vote = RankedChoiceVote {
+        ranked_choices: request.choices,
+        voter_ip,
+    };
+
+    let poll = match conn.get_poll_by_id(pollid.clone()).await {
         Ok(Some(poll)) => poll,
         Ok(None) => {
             return json!({
