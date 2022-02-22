6b383793f42d7bd858386e881b3415590aca0b21
Annika L <annika0uwu@gmail.com>
2022-02-21 20:02:40 -0800

    Frontend: Fix CI

--- frontend/middleware/dynamic-routes.ts
+++ frontend/middleware/dynamic-routes.ts
@@ -1,7 +1,11 @@
 // Based on mktcode's solution
 // see https://github.com/mktcode/dynamic-nuxt-gh-pages/blob/main/middleware/gh-pages-dynamic-routes.js
 
-/** Allows the use of #! to redirect to a Nuxt page */
+/**
+ * Allows the use of #! to redirect to a Nuxt page
+ *
+ * @param {any} context The Nuxt middleware context
+ */
 export default function(context: any) {
   const path = context.route.hash.replace('#!', '')
   if (path.length) {
