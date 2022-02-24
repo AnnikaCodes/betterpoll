deb5beeb9c4132771ac92f2f2d6d6e38d18d61f3
AnnikaCodes <AnnikaCodes@users.noreply.github.com>
2022-02-24 18:05:35 +0000

    deploy: f1d37d340d4bb407b8298bcc301172c06be3cae1

---
+++ _nuxt/static/1645725932/about/state.js
@@ -0,0 +1,1 @@
+window.__NUXT__=(function(a){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1645725932",layout:"default",error:a,serverRendered:true,routePath:"\u002Fabout",config:{API_URL:"https:\u002F\u002Fapi.betterpoll.cc\u002Fv1",DOMAIN:"betterpoll.cc",_app:{basePath:"\u002F",assetsPath:"\u002F_nuxt\u002F",cdnURL:a}}}}(null));
